a19681e104c80afd226afaac7e4395cdd8d31aa5
Abdul Waheed <abdulwaheed0776@gmail.com>
2023-11-12 19:15:38 +0500

    database

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
 from typing import Any
 from docquery import document, pipeline
+import base64
 p = pipeline('document-question-answering')
 
 app = Flask(__name__)
@@ -34,7 +35,16 @@ class FileData(db.Model):
     timeStamp = db.Column(db.String)
     fileDump = db.Column(db.JSON)
 
+def file_to_base64(file_path):
+    try:
+        with open(file_path, "rb") as file:
+            file_content = file.read()
+            base64_encoded = base64.b64encode(file_content).decode("utf-8")
+            return base64_encoded
+    except FileNotFoundError:
+        return None
 
+# //todo
 # API to get receipt
 @app.route('/receipt/<userId>', methods=['GET'])
 def get_receipt(userId):
@@ -45,15 +55,19 @@ def get_receipt(userId):
     if timestamp:
         # Fetch data by timestamp and user ID
         data = FileData.query.filter_by(userId=userId, timeStamp=timestamp).first()
+        print("Data",data)
+        base64_data = file_to_base64("files/"+data["fileName"])
         if data:
-            return jsonify(data)  # Assuming you have a serialize method in your model
+            return jsonify({"data":data,"image":base64_data})  # Assuming you have a serialize method in your model
         else:
             return jsonify({'message': 'Data not found for provided userId and timestamp'})
     else:
         # Fetch data only by user ID
         data = FileData.query.filter_by(userId=userId).all()
+        print(data)
+        base64_data = file_to_base64("files/"+data.fileName)
         if data:
-            return jsonify(data)  # Assuming you have a serialize method in your model
+            return jsonify({"data":data,"image":base64_data})  # Assuming you have a serialize method in your model
         else:
             return jsonify({'message': 'No data found for the provided userId'})
 
@@ -80,19 +94,21 @@ def extract_data_from_image(userId):
         if file.filename == "":
             return "No selected file",400
         
+        file_content = file.read()
+        base64_content = base64.b64encode(file_content).decode('utf-8')
         timeStamp=datetime.now().isoformat()
         fileName=timeStamp + file.filename
         file.save('files/'+fileName)
 
         # TODO: call python function
-        doc = document.load_document("img2.jpg")
-        lsit = ['InvoiceNumber', 'total', 'date', 'merchant']
+        doc = document.load_document("ewe.png")
+        lsit = ['InvoiceNumber', 'total', 'date', 'merchant','currency']
         data = {}
         questions = [
             "What is the invoice number?",
-            "What is the invoice total with currency?",
-            "What is the date of purchase",
-            "What is the Merchant name"
+            "What is the invoice total",
+            "What is the date",
+            "What is the Merchant name",
         ]
 
         for i,question in enumerate(questions):
@@ -101,6 +117,7 @@ def extract_data_from_image(userId):
             data[lsit[i]] = result['answer']
 
         print(data)
+        data['file'] = base64_content
         mlResponse=data 
         # extractedData={"userId":userId,"fileName":fileName,"timeStamp":timeStamp,"fileDump":mlResponse}
 
@@ -108,7 +125,7 @@ def extract_data_from_image(userId):
             userId=userId,
             fileName=fileName,
             timeStamp=timeStamp,
-            fileDump=mlResponse
+            fileDump=mlResponse,
         )
 
         db.session.add(new_file_data)
